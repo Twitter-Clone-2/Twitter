65350e12e83c615deb551deda44695be3a6cec04
Reza Amraei <amraeireza@gmail.com>
2022-06-10 15:42:26 -0700

    added login route and controller

--- server/controllers.js
+++ server/controllers.js
@@ -1,4 +1,7 @@
 const db = require("../DataBase/index");
+const bcrypt = require("bcrypt");
+const { query } = require("express");
+
 async function getAllUsers(req, res) {
   const query = `SELECT * FROM accounts;`;
   try {
@@ -9,4 +12,58 @@ async function getAllUsers(req, res) {
     res.send(e.stack);
   }
 }
-module.exports = { getAllUsers };
+
+async function getOneUser(req, res) {
+  let { id } = req.body;
+  const query = `SELECT * FROM accounts WHERE id = ${id}`;
+
+  try {
+    const results = await db.query(query);
+    res.send(results.rows);
+  } catch (e) {
+    console.error(e.stack);
+    res.send(e.stack);
+  }
+}
+
+async function register(req, res) {
+  let { first_name, last_name, email, password, username, bio, location } =
+    req.body;
+
+  bcrypt.hash(password, 10).then((hash) => {
+    password = hash;
+    db.query(
+      `INSERT INTO accounts (first_name, last_name , username, password, email, bio, location) VALUES ('${first_name}','${last_name}','${email}','${password}','${username}','${bio}','${location}');`
+    )
+      .then(() => res.status(200).send("account created"))
+      .catch((err) => console.log("err"));
+  });
+}
+//Validations for incorrect login need to be fixed/added
+async function login(req, res) {
+  let { email, password } = req.body;
+  const queryForUser = `SELECT * FROM accounts WHERE email = '${email}';`;
+
+  try {
+    const results = await db.query(queryForUser);
+    //res.send(results.rows);
+    let user = results.rows[0];
+    console.log(user);
+    //checks to see if there is a user with that email
+    if (!user) {
+      return res.status(400);
+    }
+    const passwordCheck = await bcrypt.compare(password, user.password);
+    if (!passwordCheck) {
+      console.log("passwords dont match");
+      return res.status(400);
+    }
+    console.log("hello correct user");
+    res.json({ id: user.id });
+    //checks to see if it is the correct password
+  } catch (e) {
+    console.error(e.stack);
+    res.send(e.stack);
+  }
+}
+module.exports = { getAllUsers, getOneUser, register, login };
--- server/index.js
+++ server/index.js
@@ -3,47 +3,22 @@ const db = require("../DataBase/index");
 const app = express();
 const cors = require("cors");
 const port = 8080;
-const bcrypt = require("bcrypt");
 
 app.use(cors());
 app.use(express.json());
 
-const { getAllUsers } = require("./controllers");
+const { getAllUsers, getOneUser, register, login } = require("./controllers");
 //important query SELECT * FROM replies RIGHT JOIN accounts ON accounts.id = replies.accounts_id RIGHT JOIN tweets ON tweets.id = replies.tweets_id
+
 //get all users
-// app.get("/api/users", (req, res) => {
-//   db.query("SELECT * FROM accounts")
-//     .then((response) => res.send(response.rows))
-//     .catch((e) => console.log(e));
-// });
-
-//this doesnt work
-//app.get("/api/users", () => getAllUsers);
-
 app.get("/api/users", getAllUsers);
-
+// get one user
+//probably needs a feature if user id doesnt exist
+app.get("/api/user", getOneUser);
 //login
-// app.post("/api/login", (req,res) =>{
-//   const account =
-// })
-
+app.post("/api/login", login);
 //register
-app.post("/api/register", (req, res) => {
-  console.log(req.body);
-  let { first_name, last_name, email, password, username, bio, location } =
-    req.body;
-  console.log(first_name);
-  bcrypt.hash(password, 10).then((hash) => {
-    console.log(hash);
-    password = hash;
-    db.query(
-      `INSERT INTO accounts (first_name, last_name , username, password, email, bio, location) VALUES ('${first_name}','${last_name}','${email}','${password}','${username}','${bio}','${location}');`
-    )
-      .then(() => res.status(200).send("account created"))
-      .catch((err) => console.log("err"));
-  });
-  console.log(password);
-});
+app.post("/api/register", register);
 //tweet
 
 //likes
