c55d9fccc1f418f90026bec1895858921b2de7ad
Reza Amraei <amraeireza@gmail.com>
2022-06-16 14:11:19 -0700

    chore : I meant to say registration lol

--- client/src/components/FormAccountInfo.js
+++ client/src/components/FormAccountInfo.js
@@ -1,7 +1,7 @@
 import React from "react";
 import TextField from "@mui/material/TextField";
 import "../CSS/Register.css";
-//VALIDATIONS STILL NEED TO BE ADDED
+//VALIDATIONS STILL NEED TO BE ADDED FOR EVERYTHING
 const FormAccountInfo = ({
   firstName,
   setFirstName,
