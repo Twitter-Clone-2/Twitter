89ea268e4bce37a736d287b2876b62795b41a403
Reza Amraei <amraeireza@gmail.com>
2022-07-01 15:06:51 -0700

    fixed: added start pool and end pool to each controller

--- server/controllers.js
+++ server/controllers.js
@@ -3,13 +3,16 @@ const pool = require("./dataBase/index");
 const { endPool, startPool } = require("./dataBase/index");
 //                ALL USER RELATED
 async function getAllUsers(req, res) {
+  const db = await startPool();
   const query = `SELECT * FROM accounts;`;
   try {
     const results = await db.query(query);
     res.send(results.rows);
+    endPool(db);
   } catch (e) {
     console.error(e.stack);
     res.status(400);
+    endPool(db);
   }
 }
 
@@ -25,10 +28,12 @@ async function getOneUser(req, res) {
   } catch (e) {
     console.error(e.stack);
     res.status(400);
+    endPool(db);
   }
 }
 
 async function getOneUserByEmail(req, res) {
+  const db = await startPool();
   let { email } = req.body;
   console.log(email);
   console.log(req.body);
@@ -41,10 +46,12 @@ async function getOneUserByEmail(req, res) {
   } catch (e) {
     console.error(e.stack);
     res.status(400);
+    endPool(db);
   }
 }
 
 async function register(req, res) {
+  const db = await startPool();
   let { first_name, last_name, email, password, username, bio, location } =
     req.body;
 
@@ -55,15 +62,19 @@ async function register(req, res) {
     )
       .then(() => {
         res.status(200).send("Account created");
+        endPool(db);
       })
       .catch((err) => {
         console.log(err);
+        endPool(db);
+
         return res.status(400);
       });
   });
 }
 
 async function login(req, res) {
+  const db = await startPool();
   let { email, password } = req.body;
   const queryForUser = `SELECT * FROM accounts WHERE email = '${email}';`;
 
@@ -71,14 +82,20 @@ async function login(req, res) {
     const results = await db.query(queryForUser);
 
     let user = results.rows[0];
+    endPool(db);
+
     console.log(user);
 
     if (!user) {
+      endPool(db);
+
       return res.send(false);
     }
     const passwordCheck = await bcrypt.compare(password, user.password);
     if (!passwordCheck) {
       console.log("passwords dont match");
+      endPool(db);
+
       return res.send(false);
     }
     console.log("hello correct user");
@@ -86,35 +103,43 @@ async function login(req, res) {
       user: user,
       status: true,
     });
+    endPool(db);
   } catch (e) {
     console.error(e.stack);
     res.status(400);
+    endPool(db);
   }
 }
 
 async function deleteUser(req, res) {
+  const db = await startPool();
   const { id } = req.body;
   const query = `DELETE FROM accounts WHERE id = ${id};`;
 
   try {
     const results = await db.query(query);
     res.status(200).send(`Deleted account with id of ${id}`);
+    endPool(db);
   } catch (e) {
     console.error(e.stack);
     res.status(400);
+    endPool(db);
   }
 }
 
 //                          ALL TWEET RELATED
 async function createTweet(req, res) {
+  const db = await startPool();
   const { tweet, id } = req.body;
   const query = `INSERT INTO tweets (content, accounts_id) VALUES ('${tweet}', ${id});`;
   try {
     const results = await db.query(query);
     res.status(200).send("Tweet was created succesfully");
+    endPool(db);
   } catch (e) {
     console.error(e.stack);
     res.status(400);
+    endPool(db);
   }
 }
 
@@ -125,25 +150,29 @@ async function findAllTweetsFromOneUser(req, res) {
 
   try {
     const results = await db.query(query);
-
     res.status(200).send(results);
+    endPool(db);
   } catch (e) {
     console.error(e.stack);
     res.status(400);
+    endPool(db);
   }
 }
 
 //                            Follow or Following status
 async function findFollowers(req, res) {
+  const db = await startPool();
   const { following } = req.body;
   const query = `SELECT * FROM relationship WHERE following = ${following};`;
 
   try {
     results = db.query(query);
     res.status(200).send(results);
+    endPool(db);
   } catch (e) {
     console.error(e.stack);
     res.status(400).send(false);
+    endPool(db);
   }
 }
 
@@ -156,34 +185,42 @@ async function findFollowing(req, res) {
   try {
     results = db.query(query);
     res.status(200).send(results);
+    endPool(db);
   } catch (e) {
     console.error(e.stack);
     res.status(400).send(false);
+    endPool(db);
   }
 }
 
 async function followAnotherUser(req, res) {
+  const db = await startPool();
   const { follower, following } = req.body;
   const query = `INSERT INTO relationship (follower, following) VALUES (${follower}, ${following});`;
 
   try {
     const results = await db.query(query);
     res.status(200).send(true);
+    endPool(db);
   } catch (e) {
     console.error(e.stack);
     res.status(400).send(false);
+    endPool(db);
   }
 }
 async function unFollowAnotherUser(req, res) {
+  const db = await startPool();
   const { follower, following } = req.body;
   const query = `DELETE FROM relationship WHERE follower = ${follower} AND following = ${following};`;
 
   try {
     const result = await db.query(query);
     res.status(200).send(true);
+    endPool(db);
   } catch (e) {
     console.error(e.stack);
     res.status(400).send(false);
+    endPool(db);
   }
 }
 
