c1f7749f668a60bbf5057f1a50cc98e5ed78222c
Vini Bjazevich <vinibjazevich@Vinis-iMac.local>
2022-07-03 16:43:23 -0500

    feat: styled navbar

--- client/src/components/IconNav.js
+++ client/src/components/IconNav.js
@@ -1,13 +1,13 @@
-import React, { useState } from "react";
+import React from "react";
 import HomeIcon from "@mui/icons-material/Home";
 import TwitterIcon from "@mui/icons-material/Twitter";
 import NotificationsIcon from "@mui/icons-material/Notifications";
 import ChatBubbleIcon from "@mui/icons-material/ChatBubble";
 import PersonIcon from "@mui/icons-material/Person";
 import SettingsSuggestIcon from "@mui/icons-material/SettingsSuggest";
-import "../CSS/HomePage.css";
 import { useNavigate } from "react-router-dom";
-import Post from "../components/Post";
+import Button from "@mui/material/Button";
+import "../CSS/IconNav.css";
 
 const IconNav = (props) => {
   const navigate = useNavigate();
@@ -19,16 +19,37 @@ const IconNav = (props) => {
   };
 
   return (
-    <div id="icons">
-      <TwitterIcon sx={{ fontSize: 100 }} onClick={() => handleHome()} />
-      <HomeIcon sx={{ fontSize: 100 }} onClick={() => handleHome()} />
-      <NotificationsIcon sx={{ fontSize: 100 }} />
-      <ChatBubbleIcon sx={{ fontSize: 100 }} />
-      <PersonIcon sx={{ fontSize: 100 }} onClick={() => takeToProfile()} />
-      <SettingsSuggestIcon sx={{ fontSize: 100 }} />
-      <button className="button-15" onClick={() => props.renderPost()}>
-        Create Post
-      </button>
+    <div className="iconContainer">
+      <div id="twitterButton" onClick={() => handleHome()}>
+        <TwitterIcon id="twitterIcon" className="icon" />
+      </div>
+      <Button className={"iconButton"} onClick={() => handleHome()}>
+        <HomeIcon className="icon" />
+        <div className="buttonLabel">Home</div>
+      </Button>
+      <Button className={"iconButton"}>
+        <NotificationsIcon className="icon" />
+        <div className="buttonLabel">Notifications</div>
+      </Button>
+      <Button className={"iconButton"}>
+        <ChatBubbleIcon className="icon" />
+        <div className="buttonLabel">Messages</div>
+      </Button>
+      <Button className={"iconButton"} onClick={() => takeToProfile()}>
+        <PersonIcon className="icon" />
+        <div className="buttonLabel">Profile</div>
+      </Button>
+      <Button className={"iconButton"}>
+        <SettingsSuggestIcon className="icon" />
+        <div className="buttonLabel">Settings</div>
+      </Button>
+      <Button
+        id="tweetButton"
+        variant="contained"
+        onClick={() => props.renderPost()}
+      >
+        Tweet
+      </Button>
     </div>
   );
 };
