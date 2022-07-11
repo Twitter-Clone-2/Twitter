c7d2666cc4904aa08094ca0f6665c775d568a510
Reza Amraei <amraeireza@gmail.com>
2022-07-11 14:33:13 -0700

    testing query

--- server/controllers.js
+++ server/controllers.js
@@ -304,7 +304,9 @@ async function findAllTweetsFromFollowing(req,res){
     const resultOfIds = await db.query(queryToGetFollowingIds);
     const idArr = resultOfIds.rows.map((followingObject)=> followingObject.following)
     const queryToGetAllTweets = `SELECT tweets.id , tweets.content , tweets.created_at, tweets.accounts_id, accounts.first_name, accounts.last_name , accounts.username FROM tweets LEFT JOIN accounts ON accounts.id = tweets.accounts_id WHERE accounts_id = ANY(ARRAY[${idArr}]);`;
+    
     const results = await db.query(queryToGetAllTweets);
+    console.log(results.rows);
     res.status(200).send(results.rows);
     endPool(db);
   } catch (e) {
