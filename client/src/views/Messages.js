b1e7bee7c60b3ccf2149326ffc5622709d0e1f74
Reza Amraei <amraeireza@gmail.com>
2022-07-18 23:16:32 -0700

    feat: removed React.StrictMode for now

--- client/src/views/Messages.js
+++ client/src/views/Messages.js
@@ -4,10 +4,14 @@ import io from 'socket.io-client';
 
 
 const Messages = () => {
-    //const [socket] = useState(() => io(process.env.PORT || ':8080'));
+    const [socket] = useState(() => io(":process.env.PORT" || ':8080'));
 
     useEffect(() => {
         console.log("test");
+        console.log(process.env.PORT);
+        socket.on('Welcome', data => console.log(data));
+
+        return () => socket.disconnect(true);
     }, [])
     
   return (
