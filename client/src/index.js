cac35fde00facc2b3b4a02ee85acb47a0fbaa993
Reza Amraei <amraeireza@gmail.com>
2022-09-06 22:02:31 -0700

    fixed: removed set up for picture storage with aws-amplify

--- client/src/index.js
+++ client/src/index.js
@@ -2,10 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import Amplify from "aws-amplify";
-import config from "./aws-exports";
 
-Amplify.configure(config);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
 reportWebVitals();
