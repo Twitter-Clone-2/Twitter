b55b03989c5faa4bdbc56f16f426f0893c436a19
Reza Amraei <amraeireza@gmail.com>
2023-03-29 18:53:36 -0700

    feat: added ANT to package JSON and edited create tweet div to look more like real twitter

--- client/src/utils/server_router.js
+++ client/src/utils/server_router.js
@@ -1,4 +1,4 @@
-//const route = "http://localhost:8080";
-const route = "https://ql6u1sjxf2.execute-api.us-east-1.amazonaws.com/Prod";
+const route = "http://localhost:8080";
+//const route = "https://ql6u1sjxf2.execute-api.us-east-1.amazonaws.com/Prod";
 
 module.exports = route;
